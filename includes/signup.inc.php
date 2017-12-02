32334f77ef7de4a2d44dbd5d4cab9e7a9c6b084a
Sunny77D <34179343+Sunny77D@users.noreply.github.com>
2017-12-01 23:53:46 -0500

    Initial commit

---
+++ includes/signup.inc.php
@@ -0,0 +1,55 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    include 'dhb.inc.php';
+
+    $first = mysqli_real_escape_string($conn, $_POST['first']);
+    $last = mysqli_real_escape_string($conn, $_POST['last']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $uid = mysqli_real_escape_string($conn, $_POST['uid']);
+    $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+
+    // Error handlers
+    // Check if everything is fill
+    if (empty($first) || empty($last) || empty($email) || empty($uid) ||
+        empty($pwd)){
+        header("Location: ../signup.php?signup=empty");
+        exit();
+    } else {
+        //check if char are vaild
+        if (!preg_match("/^[a-zA-Z*$]/", $first)||!preg_match("/^[a-zA-Z*$]/",
+              $last)) {
+                header("Location: ../signup.php?signup=invalid");
+                exit();
+        } else {
+              //Check if email is valid
+              if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                header("Location: ../signup.php?signup=email");
+                exit();
+              } else {
+                  $sql = "SELECT * FROM users WEHRE user_uid = '$uid'";
+                  $result = mysqli_query($conn, $sql);
+                  $result_check = mysqli_num_rows($result);
+
+                  if ($result_check > 0) {
+                      header("Location: ../signup.php?signup=usertaken");
+                      exit();
+                  } else {
+                    // Hashing Password
+                    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+                    // Insert User into the database
+                    $sql = "INSERT INTO users (user_first, user_last,
+                            user_email, user_uid, user_pwd) VALUES ('$first',
+                            '$last', '$email', '$uid', '$hashedPwd');";
+                    mysqli_query($conn, $sql);
+                    header("Location: ../signup.php?signup=success");
+                    exit();
+                  }
+              }
+        }
+    }
+} else {
+    header("Location: ../signup.php");
+    exit();
+}
+?>
